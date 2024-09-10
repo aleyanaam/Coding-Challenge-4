af27257061d1a1770e773eeb04165a19a89a9107
Aleyana McLeod <aleyanam@usf.edu>
2024-09-09 20:29:44 -0400

    Initialize inventory with product objects - Using sample data

--- inventory_management.js
+++ inventory_management.js
@@ -1,9 +1,16 @@
 // Task 1
 // The initialization of the inventory array with item name, price, quantity and when the stock is considered to be low
-let inventory = [
-    {name: "Laptop", price: 1200, quantity: 500, lowStockLevel: 150},
-    {name: "Ipad", price: 500, quantity: 600, lowStockLevel: 100},
-    {name: "IPhone", price: 900, quantity: 800, lowStockLevel: 200},
-    {name: "Television", price: 1400, quantity: 900, lowStockLevel: 300},
-    {name: "AirPods", price: 250, quantity: 600, lowStockLevel: 100},
+const inventory = [
+
+  { name: 'Laptop', price: 1200, quantity: 10, lowStockLevel: 3 },
+    
+  { name: 'Smartphone', price: 800, quantity: 5, lowStockLevel: 2 },
+    
+  { name: 'Tablet', price: 400, quantity: 7, lowStockLevel: 1 },
+    
+  { name: 'Headphones', price: 100, quantity: 15, lowStockLevel: 5 },
+    
+  { name: 'Smartwatch', price: 250, quantity: 3, lowStockLevel: 1 },
 ];
+
+
